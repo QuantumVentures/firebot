71007456ad2c058d001a18b25419d2e4c80469ab
Tommy Dang <quantumventuress@gmail.com>
2015-04-11 05:09:41 -0700

    added to db/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.create!(
+  email:      "john@gmail.com",
+  first_name: "John",
+  last_name:  "Doe",
+  password:   "12"
+)
+
+app = user.apps.create!(
+  description: "People can buy used cars from others",
+  name:        "Used Car Marketplace"
+)
+
+model = app.models.new name: "Car"
+model.update_schema :condition, required: true, type: "string"
+model.update_schema :features, required: false, type: "array"
+model.update_schema :make, required: true, type: "string"
+model.update_schema :model, required: true, type: "string"
+model.update_schema :year, required: true, type: "number"
+model.save!
+
+token = user.access_tokens.create! tokenable: app
