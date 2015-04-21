343eb5fa7921a60176d7133d647057f9784bdc54
Tommy Dang <quantumventuress@gmail.com>
2015-04-21 11:20:29 -0700

    commented out scripts for pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def index
   end
 
-  def test
-    render text: File.open("lib/tasks/#{params[:name]}.rb", "r").read
-  end
+  # def scripts
+  #   render text: File.open("lib/tasks/#{params[:name]}.rb", "r").read
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root "pages#index"
 
-  get "test" => "pages#test"
-
   # Sessions
   get "login"  => "sessions#login",  as: :login
   get "logout" => "sessions#logout", as: :logout
