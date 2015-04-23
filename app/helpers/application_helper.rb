17803143be3a43c3ab6168e8cc1e5eccf0a77aea
Tommy Dang <quantumventuress@gmail.com>
2015-04-23 09:12:13 -0700

    updated links in the header; added bread crumbs to model pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def mobile_meta_content
     "initial-scale=1.0, maximum-scale=1.0, user-scalable=0, width=320"
   end
+
+  def user_background_image(user)
+    "background-image: url(#{image_path "user-default.png"});"
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,12 +1,6 @@
 require "rails_helper"
 
 describe ApplicationHelper do
-  # describe "#body_attributes" do
-  #   it "should have a hash with class" do
-  #     expect(helper.body_attributes).to have_key :class
-  #   end
-  # end
-
   describe "#mobile_meta_content" do
     it "should have the write text" do
       expect(helper.mobile_meta_content).to eq(
@@ -15,9 +9,10 @@ describe ApplicationHelper do
     end
   end
 
-  # describe "#title" do
-  #   it "should equal Firebot when @title is nil" do
-  #     expect(helper.title).to eq "Firebot"
-  #   end
-  # end
+  describe "#user_background_image" do
+    it "should return a string" do
+      user = create :user
+      expect(helper.user_background_image user).to include "background-image"
+    end
+  end
 end
